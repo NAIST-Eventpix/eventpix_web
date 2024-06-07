21f59a297f1dec86905d48f8a66c3e0e15c4e448
Hietan <hietan.code@gmail.com>
2024-06-07 19:17:44 +0900

    Connect Next and Flask

--- backend/src/eventpix/app.py
+++ backend/src/eventpix/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/")
 def index() -> str:
-    return render_template("index.html")
+    return "Hello from Flask!"
 
 
 def main() -> None:
