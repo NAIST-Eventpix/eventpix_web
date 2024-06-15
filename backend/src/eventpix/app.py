e6036e47900991e27edee07f4137a08a0d70cb05
take0x <89313929+take0x@users.noreply.github.com>
2024-06-15 13:46:01 +0900

    Create upload method

--- backend/src/eventpix/app.py
+++ backend/src/eventpix/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,8 +10,15 @@ def index() -> str:
     return "Hello from Flask!"
 
 
+@app.route("/upload", methods=["POST"])
+def upload() -> str:
+    file = request.files["image"]
+    file.save(f"uploads/{file.filename}")
+    return "Upload successful!"
+
+
 def main() -> None:
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
 
 
 if __name__ == "__main__":
