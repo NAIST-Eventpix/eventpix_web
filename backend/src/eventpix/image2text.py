2f481a0f29db9c4022079d2397fa9c8d2b7e3103
hy20191108 <57515337+hy20191108@users.noreply.github.com>
2024-06-26 20:51:23 +0900

    Add image2text

--- backend/src/eventpix/image2text.py
+++ backend/src/eventpix/image2text.py
@@ -1,38 +1,53 @@
-from google.cloud import vision
-
 # Please setting
 # https://book.st-hakky.com/data-science/data-science-gcp-vision-api-setting/
 
-def detect_text(path):
+# this library need to read heif file
+import io
+from pillow_heif import register_heif_opener  # type: ignore[import-untyped]
+from PIL import Image
+
+register_heif_opener()
+
+
+def _get_image_binary(imagepath: str) -> bytes:
+    img_bytes = io.BytesIO()
+    img = Image.open(imagepath)
+    img = img.convert("RGB")
+    img.save(img_bytes, format="JPEG")
+    return img_bytes.getvalue()
+
+
+def detect_text(imagepath: str) -> tuple[str, str]:
     """Detects text in the file."""
+    from google.cloud import vision
 
     client = vision.ImageAnnotatorClient()
 
-    with open(path, "rb") as image_file:
-        content = image_file.read()
+    # with open(imagepath, "rb") as image_file:
+    #     content = image_file.read()
 
+    content = _get_image_binary(imagepath)
     image = vision.Image(content=content)
 
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print("Texts:")
-
-    for text in texts:
-        print(f'\n"{text.description}"')
-
-        vertices = [
-            f"({vertex.x},{vertex.y})" for vertex in text.bounding_poly.vertices
-        ]
-
-        print("bounds: {}".format(",".join(vertices)))
+    response = client.document_text_detection(
+        image=image, image_context={"language_hints": ["ja"]}
+    )
 
     if response.error.message:
         raise Exception(
             "{}\nFor more info on error messages, check: "
-            "https://cloud.google.com/apis/design/errors".format(
-                response.error.message)
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
         )
 
+    text = str(response.full_text_annotation.text)
+    json = str(response)
 
-if __name__ == '__main__':
-    detect_text('sample_image.jpg')
+    return text, json
+
+
+if __name__ == "__main__":
+    text, json = detect_text("sample_image.jpg")
+    with open("sample_image.txt", "w", encoding="utf8") as f:
+        f.write(text)
+    with open("sample_image.json", "w", encoding="utf8") as f:
+        f.write(json)
