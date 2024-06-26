64f817d05075252d694a1bd6ccb09e6152abda4d
take0x <89313929+take0x@users.noreply.github.com>
2024-06-26 17:59:27 +0900

    Update upload test to use file hash for assertion

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -9,4 +9,4 @@ def test_index(client: FlaskClient) -> None:
 def test_upload(client: FlaskClient) -> None:
     data = {"image": (open("../sample.png", "rb"), "sample.png")}
     response = client.post("/upload", data=data)
-    assert response.data == b"Upload successful!"
+    assert response.data == b"5132bfb3723cbc370fa301c7579195ea"
