9526315bc0d07350a012d30c0694edecdf3fde12
hy20191108 <57515337+hy20191108@users.noreply.github.com>
2024-06-27 14:37:19 +0900

    feat: Add function of asking ChatGPT

--- backend/src/eventpix/event_extracter.py
+++ backend/src/eventpix/event_extracter.py
@@ -1,35 +1,65 @@
 import icalendar  # type: ignore[import-untyped]
 
-# from datetime import datetime
-# from icalendar import Calendar, Event, vCalAddress, vText
-# from openai import OpenAI
-# import os
-# import pprint
-# using chatgpt api, get icalernder object
+from icalendar import Calendar
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+import re
+
+load_dotenv()  # take environment variables from .env.
+
 # https://github.com/openai/openai-python
 
-# client = OpenAI(
-#     # This is the default and can be omitted
-#     api_key=os.environ.get("OPENAI_API_KEY"),
-# )
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 
-def read(event_content: str) -> icalendar.Calendar:
-    cal = icalendar.Calendar()
-    # chat_completion = client.chat.completions.create(
-    #     messages=[
-    #         {
-    #             "role": "user",
-    #             "content": "以下の予定内容をics形式で返してください\n" + event_content,
-    #         }
-    #     ],
-    #     model="gpt-3.5-turbo",
-    # )
-    # pprint.pprint(chat_completion)
+def _get_ics_content_only(content: str) -> str:
+    pattern = re.compile(r"BEGIN:VCALENDAR.*?END:VCALENDAR", re.DOTALL)
+    matches = pattern.findall(content)
+    return str(matches[0])
+
+
+def _ask_chatgpt(content: str) -> str:
+    completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "以下の予定内容をicsファイルで返してください\n" + content,
+            }
+        ],
+        model="gpt-4o",
+    )
+
+    response = completion.choices[0].message.content
+
+    if response is None:
+        raise ValueError("ChatGPT response is None")
+
+    return response
+
+
+def _get_calender(ics_content: str) -> icalendar.Calendar:
+    cal = Calendar.from_ical(ics_content)
+    for event in cal.walk("VEVENT"):
+        print(event["SUMMARY"])
+        print(f"{event.name}")
+        print(f"{event["DTSTART"].dt}")
+        print(f"{event["DTEND"].dt}")
+        print(f"{event["DESCRIPTION"]}")
     return cal
 
 
-if __name__ == "__main__":
+def read(event_content: str) -> icalendar.Calendar:
+    response = _ask_chatgpt(event_content)
+    ics_content = _get_ics_content_only(response)
+    cal = _get_calender(ics_content)
+    return cal
+
+
+def main() -> icalendar.Calendar:
     sample_text = """ 	実施場所	時間	科目名	タイトル	詳細
 5月30日(木)
 
@@ -50,4 +80,8 @@ MOBIO
 先端科学技術ベンチャーの現在、ビジネスモデルキャンバス、
 チームわけ、アイスブレーク"""
 
-    read(sample_text)
+    return read(sample_text)
+
+
+if __name__ == "__main__":
+    print(main())
