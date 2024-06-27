a9984dfbfce4a0e5654cfbf21a13326092964434
Hietan <hietan.code@gmail.com>
2024-06-27 18:55:01 +0900

    fix actions

--- src/eventpix/app.py
+++ src/eventpix/app.py
@@ -2,7 +2,6 @@ import hashlib
 
 from dotenv import load_dotenv
 from flask import Flask, request
-from flask_cors import CORS
 
 load_dotenv(override=True)
 app = Flask(__name__)
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,6 +7,6 @@ def test_index(client: FlaskClient) -> None:
 
 
 def test_upload(client: FlaskClient) -> None:
-    data = {"image": (open("../sample.png", "rb"), "sample.png")}
+    data = {"image": (open("./sample.png", "rb"), "sample.png")}
     response = client.post("/upload", data=data)
     assert response.data == b"5132bfb3723cbc370fa301c7579195ea"
