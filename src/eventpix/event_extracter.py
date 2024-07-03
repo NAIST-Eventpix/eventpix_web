0c7d87eb106be9e4cba6b1ebeb8d01b2bfe18c6b
hy <zwwp9989@gmail.com>
2024-07-04 00:28:14 +0900

    Refactor event extraction and bux fix

--- src/eventpix/event_extracter.py
+++ src/eventpix/event_extracter.py
@@ -1,51 +1,89 @@
+import os
 import re
+import warnings
 from pathlib import Path
 
-import icalendar  # type: ignore[import-untyped]
 from dotenv import load_dotenv
-from icalendar import Calendar
+from icalendar import Calendar  # type: ignore[import-untyped]
 from openai import OpenAI
 
-openai_enabled = False
+from eventpix.event import Event
 
 load_dotenv(override=True)  # take environment variables from .env.
 
 
-def read(event_content_path: Path) -> icalendar.Calendar:
-    event_content = event_content_path.read_text(encoding="utf8")
-    response = _ask_chatgpt(event_content)
-    ics_content = _get_ics_content_only(response)
-    cal = get_calender(ics_content)
-    return cal
+class EventExtracter:
+    def __init__(self, event_content_path: Path):
+        event_content = event_content_path.read_text(encoding="utf8")
+        self.response = self._ask_chatgpt(event_content)
+        self.ics_content = self._get_ics_content_part(self.response)
 
+    @property
+    def events(self) -> list[Event]:
+        return self.ics2events(self.ics_content)
 
-def get_calender(ics_content: str) -> icalendar.Calendar:
-    return Calendar.from_ical(ics_content)
+    @staticmethod
+    def ics2events(ics_src: str | os.PathLike) -> list[Event]:
+        if isinstance(ics_src, str):
+            ics_content = ics_src
+        elif isinstance(ics_src, Path):
+            ics_content = ics_src.read_text(encoding="utf8")
+        else:
+            raise ValueError("ics_src is not str or Path")
 
+        result: list[Event] = []
 
-# https://github.com/openai/openai-python
-def _ask_chatgpt(content: str) -> str:
-    if not openai_enabled:
+        ics_content = EventExtracter.add_asia_timezone(ics_content)
+
+        try:
+            caldendar = Calendar.from_ical(ics_content)
+        except ValueError:
+            warnings.warn("Failed to parse ics_content")
+            return result
+
+        for component in caldendar.walk("VEVENT"):
+            event = Event(
+                component.get("DTSTART"),
+                component.get("DTEND"),
+                component.get("SUMMARY"),
+                component.get("DESCRIPTION"),
+                component.get("LOCATION"),
+            )
+            result.append(event)
+        return result
+
+    @staticmethod
+    def _ask_chatgpt(content: str) -> str:
+        # https://github.com/openai/openai-python
         client = OpenAI()
-    completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": "以下の予定内容をicsファイルで返してください\n" + content,
-            }
-        ],
-        model="gpt-4o",
-    )
+        completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": "以下の予定内容をicsファイルで返してください\n"
+                    + content,
+                }
+            ],
+            model="gpt-4o",
+        )
 
-    response = completion.choices[0].message.content
+        response = completion.choices[0].message.content
 
-    if response is None:
-        raise ValueError("ChatGPT response is None")
+        if response is None:
+            raise ValueError("ChatGPT response is None")
 
-    return response
+        return response
 
+    @staticmethod
+    def _get_ics_content_part(content: str) -> str:
+        pattern = re.compile(r"BEGIN:VCALENDAR.*?END:VCALENDAR", re.DOTALL)
+        matches = pattern.findall(content)
+        return str(matches[0])
 
-def _get_ics_content_only(content: str) -> str:
-    pattern = re.compile(r"BEGIN:VCALENDAR.*?END:VCALENDAR", re.DOTALL)
-    matches = pattern.findall(content)
-    return str(matches[0])
+    @staticmethod
+    def add_asia_timezone(src: str) -> str:
+        tmp = src
+        tmp = tmp.replace("DTSTART:", "DTSTART;TZID=Asia/Tokyo:")
+        tmp = tmp.replace("DTEND:", "DTEND;TZID=Asia/Tokyo:")
+        dst = tmp
+        return dst
--- tests/test_event_extracter.py
+++ tests/test_event_extracter.py
@@ -1,7 +1,7 @@
 import datetime
 
 import pytz  # type: ignore[import-untyped]
-from eventpix import event_extracter
+from eventpix.event_extracter import EventExtracter
 
 # import pandas as pd
 # import os
@@ -18,10 +18,10 @@ class TestEventExtracter:
     #     # df = pd.read_excel(xlsxpath, sheet_name=[0, "sheet2"], index_col=0)
 
     #     if len(events) >= 2:
-    #         assert events[1]["DTSTART"].dt == datetime.datetime(2021, 4, 1, 10, 0)
-    #         assert events[1]["DTEND"].dt == datetime.datetime(2021, 4, 1, 10, 0)
-    #         assert events[1]["SUMMARY"] == ""
-    #         assert events[1]["DESCRIPTION"] == ""
+    #         assert events[1].dtstart_datetime == datetime.datetime(2021, 4, 1, 10, 0)
+    #         assert events[1].dtend_datetime == datetime.datetime(2021, 4, 1, 10, 0)
+    #         assert events[1].summary == ""
+    #         assert events[1].description == ""
 
     def test_ics_content_only(self) -> None:
         content = """以下はお知らせいただいた予定内容をICS形式に変換したものです。ICSファイルはカレンダーイベントを電子的に保存および共有するための標準形式です。このファイルをカレンダーアプリケーションにインポートすることで、予定を簡単に管理できます。
@@ -74,9 +74,9 @@ DTEND;TZID=Asia/Tokyo:20230601T175000
 LOCATION:MOBIO
 END:VEVENT
 END:VCALENDAR"""
-        assert event_extracter._get_ics_content_only(content) == ics_content
+        assert EventExtracter._get_ics_content_part(content) == ics_content
 
-    def test_get_calender(self) -> None:
+    def test_get_calender1(self) -> None:
         ics_content = """BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Your Organization//Your Calendar//EN
@@ -100,34 +100,95 @@ LOCATION:MOBIO
 END:VEVENT
 END:VCALENDAR"""
         print()
-        cal = event_extracter.get_calender(ics_content)
-        events = [event for event in cal.walk("VEVENT")]
+        events = EventExtracter.ics2events(ics_content)
         asia = pytz.timezone("Asia/Tokyo")
 
-        print(events[0]["SUMMARY"])
+        print(events[0].summary)
 
-        assert events[0]["SUMMARY"] == "プロトタイピングA"
+        assert events[0].summary == "プロトタイピングA"
 
-        assert events[0]["DTSTART"].dt == asia.localize(
+        assert events[0].dtstart_datetime == asia.localize(
             datetime.datetime(2023, 5, 30, 18, 0)
         )
-        assert events[0]["DTEND"].dt == asia.localize(
+        assert events[0].dtend_datetime == asia.localize(
             datetime.datetime(2023, 5, 30, 21, 0)
         )
         assert (
-            events[0]["DESCRIPTION"]
+            events[0].description
             == "組み込みシステム1 - Raspberry Pi とLED/センサーを組み合わせたシステムとWebサービスの連携"
         )
-        assert events[0]["LOCATION"] == "奈良先端大"
-        assert events[1]["SUMMARY"] == "イノベーション創出特論1"
-        assert events[1]["DTSTART"].dt == asia.localize(
+        assert events[0].location == "奈良先端大"
+        assert events[1].summary == "イノベーション創出特論1"
+        assert events[1].dtstart_datetime == asia.localize(
             datetime.datetime(2023, 6, 1, 10, 30)
         )
-        assert events[1]["DTEND"].dt == asia.localize(
+        assert events[1].dtend_datetime == asia.localize(
             datetime.datetime(2023, 6, 1, 17, 50)
         )
         assert (
-            events[1]["DESCRIPTION"]
+            events[1].description
             == "GEIOT基礎１ - パネル：先端科学技術事業化の潮流と重要性、概要先端科学技術ベンチャーの現在、ビジネスモデルキャンバス、チームわけ、アイスブレーク"
         )
-        assert events[1]["LOCATION"] == "MOBIO"
+        assert events[1].location == "MOBIO"
+
+    def test_get_calender2(self) -> None:
+        ics_content = """BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Your Organization//Your Calendar//EN
+BEGIN:VEVENT
+UID:20230530T180000@naist.jp
+DTSTAMP:20230530T000000Z
+DESCRIPTION:組み込みシステム1 - Raspberry Pi とLED/センサーを組み合わせたシステムとWebサービスの連携
+DTEND;TZID=Asia/Tokyo:20230530T210000
+LOCATION:奈良先端大
+END:VEVENT
+BEGIN:VEVENT
+UID:20230601T103000@mobio.jp
+DTSTAMP:20230601T000000Z
+SUMMARY:イノベーション創出特論1
+DTSTART;TZID=Asia/Tokyo:20230601T103000
+LOCATION:MOBIO
+END:VEVENT
+END:VCALENDAR"""
+        events = EventExtracter.ics2events(ics_content)
+        asia = pytz.timezone("Asia/Tokyo")
+
+        assert events[0].summary == ""
+
+        assert events[0].dtstart_datetime is None
+
+        assert events[0].dtend_datetime == asia.localize(
+            datetime.datetime(2023, 5, 30, 21, 0)
+        )
+        assert (
+            events[0].description
+            == "組み込みシステム1 - Raspberry Pi とLED/センサーを組み合わせたシステムとWebサービスの連携"
+        )
+        assert events[0].location == "奈良先端大"
+        assert events[1].summary == "イノベーション創出特論1"
+        assert events[1].dtstart_datetime == asia.localize(
+            datetime.datetime(2023, 6, 1, 10, 30)
+        )
+        assert events[1].dtend_datetime is None
+
+        assert events[1].description == ""
+        assert events[1].location == "MOBIO"
+
+    def test_get_calender3(self) -> None:
+        ics_content = """何も見つかりませんでした。"""
+        events = EventExtracter.ics2events(ics_content)
+
+        assert events == []
+    
+    def test_add_asia_timezone(self) -> None:
+        src = "DTEND:20230530T210000"
+        assert EventExtracter.add_asia_timezone(src) == "DTEND;TZID=Asia/Tokyo:20230530T210000"
+
+        src = "DTEND:20230530T210000"
+        assert EventExtracter.add_asia_timezone(src) == "DTEND;TZID=Asia/Tokyo:20230530T210000"
+
+        src = "DTEND;TZID=Asia/Tokyo:20230530T210000"
+        assert EventExtracter.add_asia_timezone(src) == "DTEND;TZID=Asia/Tokyo:20230530T210000"
+
+        src = "DTSTART:20230601T103000"
+        assert EventExtracter.add_asia_timezone(src) == "DTSTART;TZID=Asia/Tokyo:20230601T103000"
